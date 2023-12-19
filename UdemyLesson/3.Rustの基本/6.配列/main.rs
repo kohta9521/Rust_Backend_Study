2de71407f99cce460d80c6c834f89815db88348a
k_95.21 <kohta0421kochi@gmail.com>
2023-12-19 23:20:58 +0900

    add new develop

--- UdemyLesson/3.Rustの基本/6.配列/main.rs
+++ UdemyLesson/3.Rustの基本/6.配列/main.rs
@@ -1,4 +1,11 @@
 fn main() {
     let l1: [i32, 3] = [1, 2, 3];
     println!("{:?}", l1);
+
+    // let i: i32 = li[0];
+
+    // let [x: i32, y: i32, _] = l1;
+
+    let l3: &[i32] = &l1[0..2];
+    println!("{:?}", l3);
 }
