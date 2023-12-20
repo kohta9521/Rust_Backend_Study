b349b0366d48dc479451a926f68e457c3fecb05b
k_95.21 <kohta0421kochi@gmail.com>
2023-12-21 01:02:06 +0900

    add new cargo fiels

---
+++ book/2.chapter/hello/target/debug/deps/hello-9f74d9df4395c284.d
@@ -0,0 +1,5 @@
+/Users/kohtakochi/Desktop/Study/rust_study/book/2.chapter/hello/target/debug/deps/hello-9f74d9df4395c284: src/main.rs
+
+/Users/kohtakochi/Desktop/Study/rust_study/book/2.chapter/hello/target/debug/deps/hello-9f74d9df4395c284.d: src/main.rs
+
+src/main.rs:
