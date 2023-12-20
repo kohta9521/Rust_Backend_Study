9eb7c9694270c018ab8dccfcd0056f8b4934ccdc
k_95.21 <kohta0421kochi@gmail.com>
2023-12-20 20:19:26 +0900

    add vector type lesson

--- UdemyLesson/3.Rustの基本/7.ベクタ型/main.rs
+++ UdemyLesson/3.Rustの基本/7.ベクタ型/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     // ベクタ型
+    // 配列とはことなり要素数を変更できる
+    // let v1: Vec!<i32> = Vec![1, 2, 3];
+    let mut v3: Vec<unknown> = Vec::new();
+
+    v3.push(1);
+    v3.push(2);
+    v3.push(3);
+
+    println!("v1[0] is {}", v1[0]);
 }
