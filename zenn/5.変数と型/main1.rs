58d1bcc5b21dfc31f357736c46c7b1293e45fe33
k_95.21 <kohta0421kochi@gmail.com>
2023-12-28 21:32:56 +0900

    add new file

---
+++ zenn/5.変数と型/main1.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let length;
+    length = 5;
+    println!("いっぺんの長さは{}です。", length);
+    println!("周辺は{}", length * 4);
+    println!("面積は{}", length * length);
+}
---
+++ zenn/6.リテラル/main3.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let big_number: i64;
+    big_number = 2147483647;
+    println!("{}", big_number);
+}
