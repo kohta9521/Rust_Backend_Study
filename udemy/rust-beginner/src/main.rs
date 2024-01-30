ed97c0e6087e14f878b14de9050ecd8a86d51f03
k_95.21 <kohta0421kochi@gmail.com>
2024-01-30 20:17:23 +0900

    add new lesson

---
+++ udemy/rust-beginner/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+    print!("hello, ");
+    print!("Rust lang");
+    print!("!");
+
+    println!("Hello, {}", "students");
+    // １行のコメント
+    /*
+        複数行のコメント
+    */
+}
