0b0f6c7c023bf4c73018a2e5ecd176a6a7f6ecb3
k_95.21 <kohta0421kochi@gmail.com>
2024-01-31 18:05:13 +0900

    add new type lesson

--- udemy/rust-beginner/src/main.rs
+++ udemy/rust-beginner/src/main.rs
@@ -1,12 +1,20 @@
 fn main() {
     println!("Hello, world!");
-    print!("hello, ");
-    print!("Rust lang");
-    print!("!");
-
     println!("Hello, {}", "students");
     // １行のコメント
     /*
         複数行のコメント
     */
+
+    // 変数宣言
+    let x: i32 = 10;
+    println!("{}", x);
+    // x = 20;
+
+    // 定数
+    // const A: i32 = 1;
+
+
+    let f: f64 = 1 as f64 + 2.0;
+    println!("{}", f);
 }
